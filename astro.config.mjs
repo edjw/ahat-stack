f646ee9e992fc679337a89df7ab7a6740fc3d7a5
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-11-09 11:07:34 +0000

    add netlify adapter for hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "hybrid",
-	integrations: [alpinejs(), tailwind()],
+  output: "hybrid",
+  integrations: [alpinejs(), tailwind()],
+  adapter: netlify()
 });
